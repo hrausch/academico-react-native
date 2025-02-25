6a919aa3c8dc6abfda12afc18eb15e3dfb173455
Herbert <hrausch@gmail.com>
2025-02-25 13:35:56 -0300

    adicionando exemplo de let e var

--- js_intro/aula01/aula_variaveis.js
+++ js_intro/aula01/aula_variaveis.js
@@ -10,13 +10,38 @@ var idade = 30;
 
 /***
  * TODO: Diferença de let e var.
+ * 
  */
+
+/**
+ * escopo de bloco vs escopo de função
+ */
+
+function exemploEscopo() {
+  if (true) {
+      let x = 10; // `let` tem escopo de bloco
+      var y = 20; // `var` tem escopo de função
+  }
+  console.log(y); // Funciona, pois `var` tem escopo de função
+  console.log(x); // Erro! `x` não é acessível fora do bloco
+}
+exemploEscopo();
+
+/**
+ * Redeclaração de Variáveis
+ */
+var nome_var = "Ana";
+var nome_var = "Carlos"; // Permitido com `var`
+console.log(nome); // Saída: "Carlos"
+
+let idade = 25;
+let idade = 30; // Erro! `let` não permite redeclaração no mesmo escopo
 
 // Strings
 let mensagem = "Hello, world!";
 
 // Números
-let idade = 30;
+// let idade = 30; //ocorrerá um erro pq idade foi declarada
 let altura = 1.75;
 
 // Booleanos
